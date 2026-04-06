5a2758eb1f8bd6ed2ae46a587bd40c5bb698d3d9
uzunkuyruk <egitimviscara@gmail.com>
2026-04-06 23:10:27 +0300

    providers eski haline dondu

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -36,7 +36,7 @@ const queryClient = new QueryClient();
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <WagmiProvider config={config} reconnectOnMount={false}>
+    <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
         {children}
       </QueryClientProvider>
