6bb23e03afa5807295fa250620f3c177f664ebcc
uzunkuyruk <egitimviscara@gmail.com>
2026-07-18 23:05:25 +0300

    logo duzeltildi

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,3 +1,11 @@
 export function Logo() {
-  return <img src="/logo.png" alt="Vaultz" width="42" height="42" />;
+  return (
+    <img
+      src="/logo.png"
+      alt="Vaultz"
+      width="44"
+      height="44"
+      style={{ borderRadius: "12px", objectFit: "cover" }}
+    />
+  );
 }
